4adcd8534fd06541928cfc3d7324bb6053ed22ad
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-11-15 14:03:34 -0700

    adding initial files

---
+++ idaho-law-decision-tree.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 13:54:01 2019
+
+@author: Ryan Gewondjan, Keaton Parkinson, Caleb Baird
+"""
+
+
+# Read in the data and preprossess
+###########################################################
+
+
+
+
+
+
+
+
+
+# Run Built-in Decision Tree Algorithm
+############################################################
+
