90b2de9407f332e98fd2d81392e0f71e13a3ca5c
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-11-25 18:19:27 -0700

    wrote out our coding tasks

--- idaho-law-decision-tree.py
+++ idaho-law-decision-tree.py
@@ -9,7 +9,25 @@ Created on Fri Nov 15 13:54:01 2019
 # Read in the data and preprossess
 ###########################################################
 
+import pandas
 
+law_data = pandas.read_csv("../idaho-law-machine-learn/template_csv_for_idaho_bills.csv")# This encoding might be necessary: encoding="ISO-8859-1")
+
+# Transform the first column to 'H' or 'S'
+
+# Reduce the topics column to the set of highest priority topics
+
+# Get Fiscal Note length make 3 buckets
+
+# Categorize session introduction into percentiles (4 percentiles)
+
+
+
+
+
+
+
+print(law_data)
 
 
 
