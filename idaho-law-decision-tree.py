0cda6c3b3ff56a3bcae5c7054e8f1999a1af286f
Caleb Baird <caleb@bicodetech.com>
2019-11-25 19:20:04 -0700

    Reduce the topics column to the single topic of most importance

--- idaho-law-decision-tree.py
+++ idaho-law-decision-tree.py
@@ -17,6 +17,25 @@ law_data = pandas.read_csv("../idaho-law-machine-learn/template_csv_for_idaho_bi
 
 # Reduce the topics column to the set of highest priority topics
 
+# Load the list of important topics
+topics_data = pd.read_csv("topics.csv")
+
+# Make it uppercase to avoid case problems
+law_data["Topics (~ delimiter)"] = law_data["Topics (~ delimiter)"].str.upper()
+topics_data.Topics = topics_data.Topics.str.upper()
+
+topics = topics_data.Topics
+
+for (index, law_topics) in enumerate(law_data["Topics (~ delimiter)"]):
+    possible_topics = law_topics.split('~')
+    possible_topic_indexs = []
+    for possible_topic in possible_topics:
+        for (i, topic) in enumerate(topics):
+            if possible_topic == topic:
+                possible_topic_indexs.append(i)
+                break
+    law_data["Topics (~ delimiter)"][index] = topics[min(possible_topic_indexs)]
+
 # Get Fiscal Note length make 3 buckets
 
 # Categorize session introduction into percentiles (4 percentiles)
